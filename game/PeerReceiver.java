302f9786b18e25f28368d6a1959cb5eb8eb12230
Fillipuster <jonas@priestyard.dk>
2019-03-05 09:50:01 +0100

    Receivers should now print what they receive.

--- game/PeerReceiver.java
+++ game/PeerReceiver.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class PeerReceiver extends Thread {
@@ -11,7 +14,16 @@ public class PeerReceiver extends Thread {
     }
 
     private void receive() {
-        while (true);
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            while (true) {
+             String str = input.readLine();
+             if (str != null && !str.isEmpty()) {
+                 System.out.println(String.format("%s says: %s", connection.getInetAddress().getHostAddress(), str));
+             }
+            }
+        } catch (IOException e) {}
     }
 
     @Override
