d5ce5c7d0ffe1f3dda5b49e48c96ced48fb9d6aa
Fillipuster <jonas@priestyard.dk>
2019-03-05 09:01:31 +0100

    Eurka2?

--- game/PeerSender.java
+++ game/PeerSender.java
@@ -20,7 +20,6 @@ public class PeerSender extends Thread {
 
     private void connect() {
         try {
-            System.out.println("Connect method: " + connection);
             if (!isConnected()) {
                 System.out.println("Calling " + ip + "...");
                 connection = new Socket();
