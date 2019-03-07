73d131ee3b9dd30551a411c154217808299aabbe
Fillipuster <jonas@priestyard.dk>
2019-03-07 09:18:52 +0100

    Commented out small remnance of old system.

--- game/Main.java
+++ game/Main.java
@@ -73,7 +73,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		Main.broadcast(new Command(Command.Type.JOIN, new String[]{Main.name}));
+		//Main.broadcast(new Command(Command.Type.JOIN, new String[]{Main.name}));
 
 		try {
 			GridPane grid = new GridPane();
